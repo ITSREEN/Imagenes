1c046b764f999fda96781aaef5eac8c719963104
ITSREEN <julivelez337@gmail.com>
2025-02-17 16:59:44 -0500

    v6

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -3,20 +3,19 @@ import './App.css';
 import Renderizardatos from './componentes/Renderizarlista'
 
 function App () {
-  const {datos, error} = Renderizardatos('https://jsonplaceholder.typicode.com/photos');
+  const {datos, error} = Renderizardatos('https://api.thecatapi.com/v1/images/search?limit=10');
   
-  // Limitar los datos a los primeros 10 elementos
-  const datosLimitados = datos?.slice(0, 5);
+ 
   
   return (
     <div className="App">
         <h1>Consumiendo Apis</h1>
       <section>
-      {datosLimitados?.map(user => (
+      {datos?.map(user => (
         <li key={user.url}>
           <p>{user.title}</p>
-          <img src={user.url} alt={user.title} />
-          <img src={user.thumbnailUrl} alt={user.title} />
+          <img src={user.url}/>
+         
         </li>
       ))}
       {error && <p>Hubo un error al cargar los datos: {error + ""}</p>} 
