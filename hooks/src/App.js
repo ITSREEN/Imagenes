480d3c551c481f3ed5c976b10637a9cb6151eda4
ITSREEN <julivelez337@gmail.com>
2025-02-18 07:10:51 -0500

    v10

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -6,18 +6,15 @@ function App () {
   const {datos:datos1, error:error1} = Renderizardatos('https://api.thecatapi.com/v1/images/search?limit=10')
   const {datos:datos2,error:error2} = Renderizardatos('https://jsonplaceholder.typicode.com/users')
   
- 
-  
   return (
     <div className="App">
         <h1>Consumiendo Apis</h1>
       <section>
-      {datos2?.map(user =><li key={user.id}>{user.name}</li>)}
+      {datos2?.map(user =><li key={user.id}>{user.id}--{user.name}--{user.username}</li>)}
       {datos1?.map(user => (
         <li key={user.url}>
           <p>{user.title}</p>
           <img src={user.url}/>
-         
         </li>
       ))}
       {error2 && <p>Hubo un error al cargar los datos: {error2 + ""}</p>} 
